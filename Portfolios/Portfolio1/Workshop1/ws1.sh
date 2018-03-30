233350c6f7ba87701d2ef531f5477121d6dd964a
Jeewan <jparajul@our.edu.au>
2018-03-30 21:39:03 +0800

    Bash Script created

---
+++ Portfolios/Portfolio1/Workshop1/ws1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#CSG6206 – Advanced Scripting
+#Workshop 1 – Version Control & Bash
+#Jeewan Parajuli(10436478)
+#Portfolio1
+echo "Bash script created to check the status of git repository"
+
