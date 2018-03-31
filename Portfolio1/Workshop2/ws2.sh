3da43beb088dea92306c22318ead161ac0ab92c9
Jeewan <jparajul@our.edu.au>
2018-03-31 20:29:37 +0800

    Date,Time and Host Name added

--- Portfolio1/Workshop2/ws2.sh
+++ Portfolio1/Workshop2/ws2.sh
@@ -3,13 +3,15 @@
 #Workshop 2 – System Information and String Formatting
 #Jeewan Parajuli(10436478)
 
-dashBorder3=$(printf -- '-%.0s' {0..30})
-echo "${dashBorder3// /-} System Report ${dashBorder3// /-}" >>output.txt
+dashBorder1=$(printf -- '-%.0s' {0..24})
+echo "${dashBorder1// /-} System Report ${dashBorder1// /-}" >>output.txt
+
+printf "Date: $(date +%d/%m/%y)Time: $(date +%T)%20sHost Name $(hostname)\n" >>output.txt
 
 
-dashBorder2=$(printf -- '-%.0s' {0..76})
+dashBorder2=$(printf -- '-%.0s' {0..64})
 echo "${dashBorder2// /-}" >>output.txt
 
-dashBorder3=$(printf -- '-%.0s' {0..31})
+dashBorder3=$(printf -- '-%.0s' {0..25})
 echo "${dashBorder3// /-}  End Report ${dashBorder3// /-}" >>output.txt
 
