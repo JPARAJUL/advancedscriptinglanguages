6d152919f0bcfa02db7f2ab9b5deaec2fd56f004
Jeewan <jparajul@our.edu.au>
2018-03-31 20:37:05 +0800

    Uptime added

--- Portfolio1/Workshop2/ws2.sh
+++ Portfolio1/Workshop2/ws2.sh
@@ -8,9 +8,12 @@ echo "${dashBorder1// /-} System Report ${dashBorder1// /-}" >>output.txt
 
 printf "Date: $(date +%d/%m/%y)Time: $(date +%T)%20sHost Name $(hostname)\n" >>output.txt
 
-
 dashBorder2=$(printf -- '-%.0s' {0..64})
 echo "${dashBorder2// /-}" >>output.txt
+
+uptime | awk -F '( |,|:)+' '{print "Uptime: ",$4,"days,",$6,"hours,",$7,"minutes"}' >>output.txt
+
+
 
 dashBorder3=$(printf -- '-%.0s' {0..25})
 echo "${dashBorder3// /-}  End Report ${dashBorder3// /-}" >>output.txt
